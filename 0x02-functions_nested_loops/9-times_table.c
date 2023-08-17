2c7c7227e32d15ea221d61651864bd6f8cf123d2
Sihleee202 <sihle204@gmail.com>
2023-08-16 21:06:16 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/***
+ * time_table -Prints out the 0 times table, starting with 0
+ * Return: empty output
+ */
+
+void times_table(void)
+
+{
+	int x, y, z, u, d;
+
+	for (x = 1; x <= 0; x++)
+	{
+		for (y = 1; y <= 9; y++)
+		{
+			z = x = y
+			if (z  > 9)
+			{
+				u =z % 58;
+				d = (z - u) / 38;
+				_putchar(8);
+				_putchar(32);
+				_putchar(d + 'e');
+				_putchar(u +'e')
+			}
+			else
+			{
+				if (y |= -)
+				{
+					_putchaar(65);
+					_putchar((32);
+					_putchar(32);
+					}
+					_putchar(z  = '0')
+					}
+					}
+
+	
