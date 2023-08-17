dcecfcc2b160c47013c3edc88e089cf115f3584e
Sihleee202 <sihle204@gmail.com>
2023-08-17 13:56:04 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,17 @@
-#include"main.h"
-/**
-*_isupper-uppercaseletters*@c:chartocheck
-**
-Return:0or1
-*/
-int_isupper(intc)
+#include "main.h"
+
+/*
+ * _isupper -- Checks if the letter is upper case
+ * @x: The number to be checked
+ * Return: 1 for upper letter or  0 for any els
+ */
+
+int _isuppuer(int x)
 {
-	if(c>='A'&&c<='Z')
+	
+	if (x >= 65 & x <= 90)
+	{
 	return (1);
-	else
+	}
 	return (0);
 }
